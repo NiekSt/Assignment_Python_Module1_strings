492d918e08632f1d119d9bc13578183392228341
NiekSt <123413210+NiekSt@users.noreply.github.com>
2023-01-30 19:58:33 +0100

    After feedback main.py
    Na feedback Maosheng

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ __human_name__ = 'strings'
 # Add your code after this line
 scorer_0 = 'Ruud Gullit'
 scorer_1 = 'Marco van Basten'
+
 goal_0 = 32
 goal_1 = 54
-scorers = str(f'{scorer_0} {goal_0}, ' + f'{scorer_1} {goal_1}')
-report = str(f'{scorer_0} scored in the {goal_0}nd minute\n{scorer_1} scored in the {goal_1}th minute')
+
+scorers = scorer_0 + ' ' + str(goal_0) + ', ' + scorer_1 + ' ' + str(goal_1)
+report = f'{scorer_0} scored in the {goal_0}nd minute\n{scorer_1} scored in the {goal_1}th minute'
+
 player = 'Ronald Koeman'
-first_name = player[0:6]
-last_name_len = len(player[-6:])
-name_short = f'{player[0]}. {player[-6:]}'
+
+index_space = player.find(' ')
+first_name = player[0:index_space]
+last_name_len = len(player[index_space+1:])
+name_short = f'{player[0]}. {player[index_space+1:]}'
+
 chant = ((f'{first_name}! ') * (len(first_name))) [:-1]
 good_chant = (chant[-1] != ' ')
